f799c98d5dfd9b2362f95e30abda6050888d04f7
abdullahsultan4026 <abdullahsultan4026@gmail.com>
2026-03-31 02:36:29 -0700

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+export default function Header() {
+  return (
+    <>
+      <h1 id="para1">Facebook</h1>
+      <p id="para2">Connects with friends at the world</p>
+      <p id="para3">around you on Facebook.</p>
+
+      <div id="classone">
+        <input type="text" placeholder="Email" id="para4" />
+        
+        <input type="password" placeholder="Password" id="para5" />
+        
+        <br />
+
+        <button id="para6">Login</button>
+
+        <p id="para7">Forget Password</p>
+
+        <button id="para8">Create new account</button>
+      </div>
+    </>
+  );
+}
